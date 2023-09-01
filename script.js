1ba8f5e9bdc864588dfb848c60fa1d003fd47408
Husban-Ali <alihusban458@gmail.com>
2023-09-01 23:31:53 +0500

    JS TASK

---
+++ script.js
@@ -0,0 +1,59 @@
+// chatpter 1
+
+
+// Q1
+// alert("welcome to my website")
+
+// Q2
+// alert("Error!Please enter a valid pasword")
+
+// Q3
+// alert("welcome to JS land \n happy codding")
+
+// Q4
+// alert("Welcome to JS land")
+// alert("Happy codding")
+
+// Q5
+// var hus = "hello...I can run JS through my web browser console";
+// console.log(alert(hus))
+
+// chapter 2
+
+// Q1
+// var username;
+
+// Q2
+// var myname="Syed Husban Ali";
+
+// Q3
+// var jsvariable = "HELLO WORLD";
+// alert(jsvariable)
+
+// Q4
+// var name = " HUSBAN ALI";
+// var age = "15 year old";
+// var certificate = "web devoloper";
+//  alert(name);
+//  alert(age);
+//  alert(certificate);
+
+// Q5
+// var pizza= "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(pizza) 
+
+// Q6
+// var email = " alihusban458@gmail.com";
+// alert("My email address is"  +email)
+
+// Q7
+// var book = "A smarter way to learn JavaScript";
+// alert(" I am trying to learn from the book" +book)
+
+// Q8
+// document.write("YAH! I can write HTML content through javascript")
+
+// Q9
+// var store ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(store)
+
